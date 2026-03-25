b2ae41c0fbb78bcd60678da2949653c107ecb046
juniorSarh <sabelohgumede@gmail.com>
2026-03-25 15:20:25 +0200

    reverted to working services as it was giving me errors to create project

--- server.ts
+++ server.ts
@@ -1,43 +1,81 @@
 require("dotenv").config();
+
 import { testConnection } from "./config/db";
+
 import express, { Express, Request, Response, NextFunction } from "express";
+
 import path from "path";
+
 import userRoutes from "./routes/userRoutes"
+
 import projectRoutes from "./routes/projectRoutes";
+
 import submissionRoutes from "./routes/submissionRoutes";
+
 import commentRoutes from "./routes/commentRoutes";
+
 import reviewRoutes from "./routes/reviewRoutes";
+
+
+
+
 
 
 
 const app:Express = express();
 
+
+
 app.use(express.json());
 
+
+
 // Serve static files from the "public" directory
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.urlencoded({ extended: true }));
 
+
+
 app.get("/", (req:Request, res:Response) => {
+
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
+
 });
+
+
+
 
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+
   res.status(400).json({success:false, message:"Not found! try again"});
+
 });
 
+
+
 const startServer= async()=>{
+
  
+
     await testConnection();
+
     app.use("/api/users", userRoutes);
+
     app.use("/api/projects", projectRoutes);
+
     app.use("/api/submissions", submissionRoutes);
-    app.use("/api/comments", commentRoutes);
-    app.use("/api/reviews", reviewRoutes);
+
     app.listen(process.env.PORT, () => {
+
         console.log(`Application is running on http://localhost:${process.env.PORT}`);  
+
         });
+
 }   
+
+
 
 export default startServer()
